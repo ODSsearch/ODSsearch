1761b3ec6f39fd4e49a332bded8eb88be996742d
Karel Tesar <karel.jr@tesar.cz>
2016-06-19 15:32:44 +0200

    Web interface first commit

---
+++ src/main/java/cz/muni/fi/pb138/odssearch/web/FileUploadController.java
@@ -0,0 +1,61 @@
+package cz.muni.fi.pb138.odssearch.web;
+
+import cz.muni.fi.pb138.odssearch.Result;
+import cz.muni.fi.pb138.odssearch.SearchInSpreadSheet;
+import cz.muni.fi.pb138.odssearch.SearchInSpreadSheetImpl;
+import org.jopendocument.dom.spreadsheet.SpreadSheet;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.List;
+
+@Controller
+public class FileUploadController {
+
+    @RequestMapping(method = RequestMethod.GET, value = "/")
+    public String searchForm(Model model) {
+
+        return "uploadForm";
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/")
+    public String handleFileUpload(@RequestParam("searchExpression") String searchExpression,
+                                   @RequestParam("file") MultipartFile file,
+                                   RedirectAttributes redirectAttributes) throws IOException {
+
+
+        if (!file.isEmpty()) {
+            try {
+                File source = multipartToFile(file);
+                SearchInSpreadSheet spreadSheet = new SearchInSpreadSheetImpl(SpreadSheet.createFromFile(source), searchExpression);
+
+                List<Result> results = spreadSheet.searchAllSheets();
+                redirectAttributes.addFlashAttribute("results", results);
+
+            } catch (Exception e) {
+                redirectAttributes.addFlashAttribute("message", "You failed to search " + searchExpression + " => " + e.getMessage());
+            }
+        } else {
+            redirectAttributes.addFlashAttribute("message", "You failed to search " + searchExpression + " because the file was empty");
+        }
+        return "redirect:/";
+    }
+
+
+    private File multipartToFile(MultipartFile multipart) throws IllegalStateException, IOException {
+        File convFile = new File(multipart.getOriginalFilename());
+        convFile.createNewFile();
+        FileOutputStream fos = new FileOutputStream(convFile);
+        fos.write(multipart.getBytes());
+        fos.close();
+        return convFile;
+    }
+}
